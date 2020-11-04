e1caf57747469dac85ea77a9b00e55ae04df6889
cronap <cronap@student.chalmers.se>
2020-11-04 14:33:45 +0100

    explanation

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello betyder hej, World");
+        System.out.println("Hello means hej in Swedish, World");
     }
 }
