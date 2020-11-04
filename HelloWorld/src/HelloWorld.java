b8b3c8f56de4f94d206cc51eaeabfc895ebbe56c
cronap <cronap@student.chalmers.se>
2020-11-04 15:03:59 +0100

    hugo

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("HUGO BOSS DAHLGREN");
+        System.out.println("HUGO NOOB DAHLGREN");
     }
 }
