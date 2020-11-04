887ffc03516be803d461876719cb98d44eb86a32
hugodahlgren <h.dahlgren@icloud.com>
2020-11-04 14:41:45 +0100

    Hugo Boss

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello means hej in Swedish, World");
+        System.out.println("HUGO BOSS DAHLGREN");
     }
 }
